486facc07be17ef057edb37e06685ca3e4d84ae7
joshuamorganx <cwdxju821@outlook.com>
2025-12-23 12:25:29 +0800

    Wire deployment for the oracle

--- deploy/deploy.ts
+++ deploy/deploy.ts
@@ -5,13 +5,14 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployer } = await hre.getNamedAccounts();
   const { deploy } = hre.deployments;
 
-  const deployedFHECounter = await deploy("FHECounter", {
+  const deployedFHEOracle = await deploy("FHEOracle", {
     from: deployer,
+    args: [deployer],
     log: true,
   });
 
-  console.log(`FHECounter contract: `, deployedFHECounter.address);
+  console.log(`FHEOracle contract: `, deployedFHEOracle.address);
 };
 export default func;
-func.id = "deploy_fheCounter"; // id required to prevent reexecution
-func.tags = ["FHECounter"];
+func.id = "deploy_fheOracle"; // id required to prevent reexecution
+func.tags = ["FHEOracle"];
